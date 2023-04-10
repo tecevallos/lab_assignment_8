6c63547de98cfe7d6b95c685e65e5dde836569cc
Tomas Enrique Cevallos <120340332+tecevallos@users.noreply.github.com>
2023-04-09 22:39:48 -0400

    Update sort.c

--- sort.c
+++ sort.c
@@ -4,27 +4,24 @@
 
 int extraMemoryAllocated;
 
-// implements heap sort
-// extraMemoryAllocated counts bytes of memory allocated
 void heapify(int arr[], int n, int i)
 {
-    int largest = i; // Initialize largest as root
-    int l = 2 * i + 1; // left = 2*i + 1
-    int r = 2 * i + 2; // right = 2*i + 2
- 
-    // If left child is larger than root
-    if (l < n && arr[l] > arr[largest])
-        largest = l;
- 
-    // If right child is larger than largest so far
-    if (r < n && arr[r] > arr[largest])
-        largest = r;
- 
-    // If largest is not root
-    if (largest != i) {
+    int largest = i;
+    int left = 2*i + 1;
+    int right = 2*i + 2;
+
+    if (left < n && arr[left] > arr[largest])
+        largest = left;
+
+    if (right < n && arr[right] > arr[largest])
+        largest = right;
+
+    if (largest != i)
+    {
         int temp = arr[i];
         arr[i] = arr[largest];
         arr[largest] = temp;
+
         heapify(arr, n, largest);
     }
 }
@@ -33,62 +30,109 @@ void heapSort(int arr[], int n)
 {
     for (int i = n / 2 - 1; i >= 0; i--)
         heapify(arr, n, i);
- 
-    for (int i = n - 1; i > 0; i--) {
+
+    for (int i = n - 1; i > 0; i--)
+    {
         int temp = arr[0];
         arr[0] = arr[i];
         arr[i] = temp;
+
         heapify(arr, i, 0);
     }
 }
 
-
-// implement merge sort
-// extraMemoryAllocated counts bytes of extra memory allocated
-void merge(int pData[], int l, int m, int r, int temp[]) 
-{ 
-    int i = l, j = m + 1, k = 0; 
-  
-    while (i <= m && j <= r) { 
-        if (pData[i] <= pData[j]) 
-            temp[k++] = pData[i++]; 
-        else
-            temp[k++] = pData[j++]; 
-    } 
-  
-    while (i <= m) 
-        temp[k++] = pData[i++]; 
-  
-    while (j <= r) 
-        temp[k++] = pData[j++]; 
-  
-    for (i = l, k = 0; i <= r; i++, k++) 
-        pData[i] = temp[k]; 
-}
-
-void mergeSortHelper(int pData[], int l, int r, int temp[]) 
-{ 
-    if (l < r) { 
-        int m = l + (r - l) / 2; 
-  
-        mergeSortHelper(pData, l, m, temp); 
-        mergeSortHelper(pData, m + 1, r, temp); 
-  
-        merge(pData, l, m, r, temp); 
-    } 
-} 
-
-void mergeSort(int pData[], int l, int r)
+void merge(int arr[], int l, int m, int r)
 {
-    int *temp = (int *)malloc(sizeof(int) * (r-l+1));
-    extraMemoryAllocated += sizeof(int) * (r-l+1);
-    if (temp == NULL)
+    int i, j, k;
+    int n1 = m - l + 1;
+    int n2 = r - m;
+
+    int L[n1], R[n2];
+
+    for (i = 0; i < n1; i++)
+        L[i] = arr[l + i];
+    for (j = 0; j < n2; j++)
+        R[j] = arr[m + 1 + j];
+
+    i = 0;
+    j = 0;
+    k = l;
+
+    while (i < n1 && j < n2)
     {
-        printf("Cannot allocate memory\n");
-        exit(-1);
+        if (L[i] <= R[j])
+        {
+            arr[k] = L[i];
+            i++;
+        }
+        else
+        {
+            arr[k] = R[j];
+            j++;
+        }
+        k++;
     }
-    
-    mergeSortHelper(pData, l, r, temp);
-    
-    free(temp);
+
+    while (i < n1)
+    {
+        arr[k] = L[i];
+        i++;
+        k++;
+    }
+
+    while (j < n2)
+    {
+        arr[k] = R[j];
+        j++;
+        k++;
+    }
 }
+
+void mergeSort(int arr[], int l, int r)
+{
+    if (l < r)
+    {
+        int m = l + (r - l) / 2;
+
+        mergeSort(arr, l, m);
+        mergeSort(arr, m + 1, r);
+
+        int* tempArr = (int*)malloc(sizeof(int) * (r - l + 1));
+        if (tempArr == NULL)
+        {
+            printf("Cannot allocate memory\n");
+            exit(-1);
+        }
+
+        int i = l;
+        int j = m + 1;
+        int k = 0;
+
+        while (i <= m && j <= r)
+        {
+            if (arr[i] < arr[j])
+            {
+                tempArr[k++] = arr[i++];
+            }
+            else
+            {
+                tempArr[k++] = arr[j++];
+            }
+        }
+
+        while (i <= m)
+        {
+            tempArr[k++] = arr[i++];
+        }
+
+        while (j <= r)
+        {
+            tempArr[k++] = arr[j++];
+        }
+
+        for (int p = 0; p < k; p++)
+        {
+            arr[l + p] = tempArr[p];
+        }
+
+        free(temp
